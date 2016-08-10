c1aaf2d76fa311bd1e3dfb2825bf9b6bc3c7d600
Kent O. Johnson <kentoj@gmail.com>
2016-08-10 10:37:30 -0600

    Post-script execution of module if run as script

--- text_processor.py
+++ text_processor.py
@@ -1,19 +1,23 @@
 from urllib.request import urlopen
 
-with urlopen('http://sixty-north.com/c/t.txt') as story:
-    story_words = []
-    for line in story:
-        line_words = line.decode('utf-8').split()
-        for word in line_words:
-            story_words.append(word)
-    wordCursor = 0
-    print("Word Count", len(story_words))
-    while wordCursor < len(story_words):
-        paragraphCursor = 0
-        while paragraphCursor < 6:
-            if (wordCursor + paragraphCursor) == len(story_words):
-                break
-            print(story_words[wordCursor+paragraphCursor], end=' ')
-            paragraphCursor += 1
-        wordCursor += paragraphCursor
-        print()
+def fetch_words():
+    with urlopen('http://sixty-north.com/c/t.txt') as story:
+        story_words = []
+        for line in story:
+            line_words = line.decode('utf-8').split()
+            for word in line_words:
+                story_words.append(word)
+        wordCursor = 0
+        print("Word Count", len(story_words))
+        while wordCursor < len(story_words):
+            paragraphCursor = 0
+            while paragraphCursor < 6:
+                if (wordCursor + paragraphCursor) == len(story_words):
+                    break
+                print(story_words[wordCursor+paragraphCursor], end=' ')
+                paragraphCursor += 1
+            wordCursor += paragraphCursor
+            print()
+
+if __name__ == '__main__':
+    fetch_words()
