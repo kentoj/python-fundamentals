c28ffa846a073f64af5f1576f8732383658c95ca
Kent O. Johnson <kentoj@gmail.com>
2016-08-10 10:48:49 -0600

    move printing logic to separate method

--- text_processor.py
+++ text_processor.py
@@ -4,31 +4,31 @@ from urllib.request import urlopen
 def fetch_words():
     with urlopen('http://sixty-north.com/c/t.txt') as story:
         story_words = []
-        word_list = ''
         for line in story:
             line_words = line.decode('utf-8').split()
             for word in line_words:
                 story_words.append(word)
-        wordCursor = 0
-        print("Word Count", len(story_words))
-        while wordCursor < len(story_words):
-            paragraphCursor = 0
-            while paragraphCursor < 6:
-                if (wordCursor + paragraphCursor) == len(story_words):
-                    break
-                word_list += story_words[wordCursor + paragraphCursor] + ' '
-                paragraphCursor += 1
-            wordCursor += paragraphCursor
-            word_list += '\n'
-        return word_list
+        return story_words
 
 
-def print_words(word_list):
+def print_items(story_words):
+    word_list = ''
+    word_cursor = 0
+    print("Word Count", len(story_words))
+    while word_cursor < len(story_words):
+        paragraphCursor = 0
+        while paragraphCursor < 6:
+            if (word_cursor + paragraphCursor) == len(story_words):
+                break
+            word_list += story_words[word_cursor + paragraphCursor] + ' '
+            paragraphCursor += 1
+        word_cursor += paragraphCursor
+        word_list += '\n'
     print(word_list)
 
 
 def main():
-    print(fetch_words())
+    print_items(fetch_words())
 
 if __name__ == '__main__':
     main()
