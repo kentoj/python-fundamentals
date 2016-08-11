933fcfff7a9c63b03e13b0bb7756f0530603c556
Kent O. Johnson <kentoj@gmail.com>
2016-08-11 16:40:50 -0600

    Refactor to ensure closing and also use list comprehension

--- series.py
+++ series.py
@@ -4,13 +4,11 @@ import sys
 
 
 def read_series(filename):
-    f = open(filename, mode='rt', encoding='utf-8')
-    series = []
-    for line in f:
-        a = int(line.strip())
-        series.append(a)
-    f.close()
-    return series
+    try:
+        f = open(filename, mode='rt', encoding='utf-8')
+        return [int(line.strip()) for line in f]
+    finally:
+        f.close()
 
 
 def main(filename):
