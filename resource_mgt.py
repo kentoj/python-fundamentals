e2915202c35f241bd8e177fbc5aea4b1b04a815d
Kent O. Johnson <kentoj@gmail.com>
2016-08-11 16:12:06 -0600

    Close file, open, read, and print, and append

--- resource_mgt.py
+++ resource_mgt.py
@@ -7,3 +7,18 @@ file_to_write = open('wasteland-copy.txt', mode='wt', encoding='utf-8')
 
 file_to_write.write("What are the roots that clutch, ")
 file_to_write.write('what branches grow\n')
+file_to_write.close()
+
+file_reading = open('wasteland.txt', mode='rt', encoding='utf-8')
+for line in file_reading.readlines():
+    print(line)
+
+
+file_to_append = open('wasteland-copy.txt', mode='at', encoding='utf-8')
+file_to_append.writelines(
+    ['Son of man,\n',
+     'You cannot say, or guess, ',
+     'for you know only,\n',
+     'A heap of broken images, ',
+     'where the sun beats\n'])
+file_to_append.close()
