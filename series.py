0af3258cd3a6ad4a75aad8c967d72f7c1b755624
Kent O. Johnson <kentoj@gmail.com>
2016-08-11 16:42:49 -0600

    Refactor to use with-block

--- series.py
+++ series.py
@@ -4,11 +4,8 @@ import sys
 
 
 def read_series(filename):
-    try:
-        f = open(filename, mode='rt', encoding='utf-8')
+    with open(filename, mode='rt', encoding='utf-8') as f:
         return [int(line.strip()) for line in f]
-    finally:
-        f.close()
 
 
 def main(filename):
