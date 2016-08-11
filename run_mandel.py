e019ce982325a6284e844df3c9a5f8172f494ba3
Kent O. Johnson <kentoj@gmail.com>
2016-08-11 17:38:03 -0600

    Add a main runner for mandel

--- run_mandel.py
+++ run_mandel.py
@@ -1,6 +1,12 @@
 import fractal
 import bmp
 
-pixels = fractal.mandelbrot(488, 256)
 
-bmp.write_grayscale('mandel.bmp', pixels)
+def main():
+    pixels = fractal.mandelbrot(488, 256)
+
+    bmp.write_grayscale('mandel.bmp', pixels)
+
+
+if __name__ == '__main__':
+    main()
