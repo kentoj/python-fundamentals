be1c3bfdbfab67b158ed488022612e800f660acb
Kent O. Johnson <kentoj@gmail.com>
2016-08-11 12:07:05 -0600

    Add validation to flight number

--- airtravel.py
+++ airtravel.py
@@ -3,7 +3,21 @@
 
 class Flight:
     def __init__(self, number):
+        if not number[:4].isalpha():
+            raise ValueError("No airline code in '{}'".format(number))
+
+        if not number[:4].isupper():
+            raise ValueError("Invalid airline code'{}'".format(number))
+
+        if not (number[4:].isdigit() and int(number[4:]) <= 999999):
+            raise ValueError("Invalid route number '{}'".format(number))
+
         self._number = number
 
     def number(self):
         return self._number
+
+    def airline(self):
+        return self._number[:4]
+
+
