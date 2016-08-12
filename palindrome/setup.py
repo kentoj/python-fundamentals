f7b93228c18d1553aad11580b7d2f42c999da376
Kent O. Johnson <kentoj@gmail.com>
2016-08-11 19:41:06 -0600

    Add setup.py for palindrome module

---
+++ palindrome/setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+    name='palindrome',
+    version='0.1',
+    py_modules=['palindrome'],
+
+    # Metadata
+    author='Kent Johnson',
+    author_email='kentoj@gmail.com',
+    description='A module for determining whether a number is a palindrome.',
+    license='Public domain',
+    keywords='example',
+)
